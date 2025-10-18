467c5d5430ca61df04d2623b8c2678122879ee4f
jtothepro <168333109+jtothepro@users.noreply.github.com>
2025-10-18 12:40:42 +0200

    Added Footer to code

---
+++ react-week4/src/Footer.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import axios from "axios";
+import "./App.css";
+
+export default function Footer() {
+  return (
+    <div className="App">
+      <footer>
+        This was coded by Jenny P and is{" "}
+        <a href="https://github.com/jtothepro" target="_blank">
+          open sourced by GitHub
+        </a>
+      </footer>
+    </div>
+  );
+}
