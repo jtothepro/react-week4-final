be0d7755edccc36867ce1daeff78a2d051023098
jtothepro <168333109+jtothepro@users.noreply.github.com>
2025-11-03 16:59:57 +0100

    Setup of ForecastDay component to simplify the forecast code

--- react-week4/src/WeatherForecast.jsx
+++ react-week4/src/WeatherForecast.jsx
@@ -3,29 +3,33 @@ import "./WeatherForecast.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay.jsx";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecastData, setForecastData] = useState(null);
   function handleResponse(response) {
+    setForecastData(response.data.daily);
+    setLoaded(true);
     console.log(response.data);
   }
 
-  const apiKey = "85be9c7bad2eb4fafc3fe0e35t2o0c3e";
-  let city = props.city;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">5-day Weather Forecast</div>
-      <div className="col">
-        <div className="WeatherForecastDay">Thu</div>{" "}
-        <div className="Icon">Icon</div>
-        <div className="WeatherForecastTemp">
-          <span className="Max">19°</span>
-          {""} <span className="Min">10°</span>
+  if (loaded) {
+    console.log(forecastData);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">5-day Weather Forecast</div>
+        <div className="col">
+          <WeatherForecastDay data={forecastData[0]} />
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "85be9c7bad2eb4fafc3fe0e35t2o0c3e";
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
---
+++ react-week4/src/WeatherForecastDay.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let maxTemperature = Math.round(props.data.temperature.maximum);
+    return `${maxTemperature}`;
+  }
+
+  function minTemperature() {
+    let minTemperature = Math.round(props.data.temperature.minimum);
+    return `${minTemperature}`;
+  }
+
+  function forecastDay() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
+  return (
+    <div>
+      <div className="WeatherForecastDay">{forecastDay()}</div>{" "}
+      <div className="Icon">
+        <img
+          src={props.data.condition.icon_url}
+          alt={props.data.condition.description}
+          width={48}
+          height={48}
+        />
+      </div>
+      <div className="WeatherForecastTemp">
+        <span className="Max">{maxTemperature()}°</span>
+        {""} <span className="Min">{minTemperature()}°</span>
+      </div>
+    </div>
+  );
+}
